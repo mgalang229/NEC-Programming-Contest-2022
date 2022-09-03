8c83857303ffc19b89098bfa64463779866c9ec0
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-09-03 21:43:07 +0800

    Update and rename Problem1.java to A.java

--- A.java
+++ A.java
@@ -1,7 +1,7 @@
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Problem1 {
+public class A {
 	
 	public static void main(String[] args) {
 		Scanner fs = new Scanner(System.in);
