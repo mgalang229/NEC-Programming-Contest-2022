5d6828f80800bbcb35d50a56cb8fe38106e46aa0
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-09-03 21:42:08 +0800

    Create Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,27 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Problem1 {
+	
+	public static void main(String[] args) {
+		Scanner fs = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+//		T = fs.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			String s = fs.next();
+			String[] days = new String[] {"", "Monday", "Tuesday",
+					"Wednesday", "Thursday", "Friday", "Saturday"};
+			int day = 0;
+			for (int i = 0; i < 7; i++) {
+				if (s.equals(days[i])) {
+					day = i + 1;
+					break;
+				}
+			}
+			out.println(7 - day);
+		}
+		fs.close();
+		out.close();
+	}
+}
